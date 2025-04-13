6e838322cf7930d8af6bd6e0e0dbfcc69553a781
v.godlevskiy <avatarsik6699@gmail.com>
2025-04-13 17:00:29 +0300

    Add Jest configuration file and update package.json for improved testing setup: Introduce jest.config.ts for centralized Jest settings, including coverage thresholds and test environment configurations. Modify test:container script to utilize the new Jest config for consistency in testing across environments.

---
+++ backend/jest.config.ts
@@ -0,0 +1,52 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+
+	rootDir: ".",
+	moduleNameMapper: {
+		"^src/(.*)$": "<rootDir>/src/$1",
+	},
+	testRegex: ".*\\.(spec|test)\\.ts$",
+	moduleFileExtensions: ["js", "json", "ts"],
+	modulePaths: ["<rootDir>"],
+
+	testPathIgnorePatterns: ["/node_modules/", "/dist/", "/coverage/"],
+	watchPathIgnorePatterns: ["/node_modules/", "/dist/", "/coverage/"],
+	transformIgnorePatterns: ["node_modules/(?!(ora)/)", "node_modules/(?!@nestjs)/"],
+
+	cache: true,
+	cacheDirectory: ".jest-cache",
+	verbose: true,
+	maxWorkers: "50%",
+	testTimeout: 30000,
+	bail: 1,
+	clearMocks: true,
+	resetMocks: true,
+	restoreMocks: true,
+
+	collectCoverageFrom: [
+		"**/*.(t|j)s",
+		"!**/*.d.ts",
+		"!**/node_modules/**",
+		"!**/dist/**",
+		"!**/coverage/**",
+		"!**/jest.config.*",
+	],
+	coverageDirectory: "../coverage",
+	coverageReporters: ["text", "lcov", "html"],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
+
+	globalTeardown: "<rootDir>/src/shared/tests/teardown.ts",
+	globalSetup: "<rootDir>/src/shared/tests/setup.ts",
+	setupFilesAfterEnv: ["<rootDir>/src/shared/tests/global-hooks-for-every-test.ts"],
+};
+export default config;
